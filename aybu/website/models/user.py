b56a66767cd76a2493f2045440f4bcecc5efba1f
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-07-22 10:14:16 +0200

    Translated 'User' and 'Group' model from Elixir to SQLAlchemy.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110722081416-dd166r33lr9ura22

--- aybu/website/models/user.py
+++ aybu/website/models/user.py
@@ -1,49 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Copyright © 2010 Asidev s.r.l. - www.asidev.com
-"""
+""" Copyright © 2010 Asidev s.r.l. - www.asidev.com """
 
-from elixir import Entity, Field, Unicode
-from elixir import using_options
-from elixir import ManyToMany
-from aybu.cms.model.types import SHA1
-
-
-class User(Entity):
-    username = Field(Unicode(255), primary_key=True)
-    password = Field(SHA1(), required=True)
-    groups = ManyToMany('Group', tablename="users_groups")
-
-    using_options(tablename="users")
-
-    def is_admin(self):
-        if "admin" in (g.name for g in self.groups):
-            return True
-        return False
-
-    def __str__(self):
-        return "<User %s>" % (self.username)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __eq__(self, other):
-        return self.username == other.username
+from aybu.website.models import Base
+from aybu.website.models.types import SHA1
+from logging import getLogger
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Unicode
+from sqlalchemy.orm import backref
+from sqlalchemy.orm import relationship
 
 
-class Group(Entity):
-    name = Field(Unicode(32), primary_key=True)
-    users = ManyToMany('User', tablename="users_groups")
+__all__ = []
 
-    using_options(tablename="groups")
+log = getLogger(__name__)
 
-    def __str__(self):
-        return "<Group {0}>".format(self.name)
 
-    def __repr__(self):
-        return self.__str__()
+users_groups = Table('users_groups',
+                     Base.metadata,
+                     Column('users_username',
+                            Unicode(255),
+                            ForeignKey('users.username',
+                                       onupdate="cascade",
+                                       ondelete="cascade")),
+                            Column('groups_name',
+                                   Unicode(32),
+                                   ForeignKey('groups.name',
+                                              onupdate="cascade",
+                                              ondelete="cascade"))
+                            )
 
-    def __eq__(self, other):
-        return self.name == other.name
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+    username = Column(Unicode(255), primary_key=True)
+    password = Column(SHA1(), nullable=False)
+
+    groups = relationship('Group', seondary=users_groups, backref='users')
+
+
+class Group(Base):
+
+    name = Column(Unicode(32), primary_key=True)
+
