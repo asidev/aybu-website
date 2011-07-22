17468685721262f7f0901cfe8b0404ecd80e5e17
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-07-22 19:19:08 +0200

    Completed 'The Smart Traversal': the function 'get_root_resource' build a resource tree without any query. This version is based on new 'url' argument in NodeInfo model.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110722171908-8a0k05fqenxylcd9

--- aybu/website/resources.py
+++ aybu/website/resources.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from aybu.website.models.language import Language
+from aybu.website.models.node import NodeInfo
 import logging
 
 
@@ -9,16 +11,11 @@ __all__ = []
 log = logging.getLogger(__name__)
 
 
-class NoLanguage(object):
-    """ This class is the context when no language nor nodes are specified."""
-    pass
-
-
 def get_root_resource(request):
 
     log.debug('get_root_resource: %s', request.path_info)
 
-    url_parts = [url_part
+    url_parts = [(url_part, Resource())
                  for url_part in request.path_info.strip('/').split('/') 
                  if url_part]
 
@@ -29,35 +26,41 @@ def get_root_resource(request):
         log.debug('Return NoLanguage context.')
         return NoLanguage()
 
-    lang = url_parts.pop(0)
-
-    if not url_parts:
+    if len(url_parts) == 1:
         # URL is like '/{lang}'.
-        #criterion = Language.lang.ilike(lang)
-        #query = request.db_session.query(Language).filter(criterion)
-        return {lang: lang}#query.first()}
+        log.debug('Return Language %s (if exists) or None.', lang)
+        return Language.get_by_lang(request.db_session, url_parts[0])
 
-    resource = {}
-    resource[lang] = {}
+    else:
+        # URL is like '/{lang}/{node}/[...]/{page}[.ext]
+        # Get the NodeInfo from database using path_info.
+        url_parts[-1] = (url_parts[-1][0],
+                         NodeInfo.get_by_url(request.db_session,
+                                             request.path_info))
 
-    # for user, address in session.query(User, adalias).join(adalias, User.addresses):
-    queries = []
-    pivot = resource[lang]
+    # Create the resources tree.
+    # The last element in resources tree is the request context.
+    tmp = root = {}
+    for url_part, resource in url_parts:
+        tmp[url_part] = resource
+        tmp = tmp[url_part]
+        key = url_part
 
-    for url_part in url_parts:
+    log.debug('Resource is: %s', root)
+    log.debug('Pivot is: %s', tmp)
+    log.debug('Key is: %s', key)
 
-        pivot[url_part] = {}
-        pivot = pivot[url_part]
+    return root
 
-        #criterion = and_(NodeInfo.url_part.ilike(url_part),
-        #                 NodeInfo.lang.has(NodeInfo.lang.ilike(lang)))
-        #query = request.db_session.query(NodeInfo).filter(criterion)
-        #NodeInfoAlias = aliased(NodeInfo, query.subquery())
-        #query = request.db_session.query(Node).join(NodeInfoAlias, Node.translations)
 
-    log.debug(resource)
+class NoLanguage(object):
+    """ This class is the context when no language nor nodes are specified."""
+    pass
 
-    if url_parts:
-        nodes = url_parts
 
-    return resource
+class Resource(dict):
+    """
+        This class is a basic resource.
+        it is needed to build ACL, do NOT use for requests context!
+    """
+    pass
