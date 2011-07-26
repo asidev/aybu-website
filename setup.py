5d1fb0c86629ed9be27f7f5345532d2d5d2556cf
Giacomo Bagnoli <g.bagnoli@asidev.com>
2011-07-26 17:52:58 +0200

    Fixed pep8 on setup.py
    --HG--
    extra : convert_revision : g.bagnoli%40asidev.com-20110726155258-1802ik51j5t9yt41

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ description = 'aybu-website'
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
-long_description = README + '\n\n' +  CHANGES
+long_description = README + '\n\n' + CHANGES
 
 classifiers = ["Programming Language :: Python",
                "Framework :: Pyramid", "Topic :: Internet :: WWW/HTTP",
@@ -37,9 +37,9 @@ paster_plugins = ['pyramid']
 namespace_packages = ['aybu']
 
 setup(name=name, version=version, description=description,
-      long_description=long_description, classifiers=classifiers, author=author,
-      author_email=author_email, url=url, keywords=keywords,
+      long_description=long_description, classifiers=classifiers,
+      author=author, author_email=author_email, url=url, keywords=keywords,
       packages=find_packages(), include_package_data=include_package_data,
       zip_safe=zip_safe, install_requires=requires, tests_require=requires,
-      test_suite=test_suite, entry_points = entry_points,
+      test_suite=test_suite, entry_points=entry_points,
       paster_plugins=paster_plugins, namespace_packages=namespace_packages)
