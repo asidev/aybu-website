ba460af5d6a4e0af703fb9a591f67183ff190fb4
Giacomo Bagnoli <g.bagnoli@asidev.com>
2011-07-26 17:37:08 +0200

    Use a namedtuple instead of a tuple during traversal
    --HG--
    extra : convert_revision : g.bagnoli%40asidev.com-20110726153708-qi1n89h5ehiyjagj

--- aybu/website/resources.py
+++ aybu/website/resources.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
 from aybu.website.models.language import Language
 from aybu.website.models.node import NodeInfo
 import logging
@@ -11,6 +12,9 @@ __all__ = []
 log = logging.getLogger(__name__)
 
 
+UrlPart = namedtuple('UrlPart', ['part', 'resource'])
+
+
 def get_root_resource(request):
 
     log.debug('get_root_resource: %s', request.path_info)
@@ -18,7 +22,7 @@ def get_root_resource(request):
     # Getting url_parts and for each part associating a Resource
     # On request.path_info applying strip('/') remvoe the initial / so
     # with the following split('/') we obtain a list just with parts
-    url_parts = [(url_part, Resource())
+    url_parts = [UrlPart(part=url_part, resource=Resource())
                  for url_part in request.path_info.strip('/').split('/')
                  if url_part]
 
@@ -29,20 +33,24 @@ def get_root_resource(request):
         log.debug('Return NoLanguage context.')
         return NoLanguage()
 
-    request.language = Language.get_by_lang(request.db_session, url_parts[0][0])
+    request.language = Language.get_by_lang(request.db_session,
+                                            url_parts[0].part)
 
     if len(url_parts) == 1:
         # URL is like '/{lang}'.
-        log.debug('Get Context by Language %s.', url_parts[0][0])
-        url_parts[0] = (url_parts[0][0], request.language)
+        log.debug('Get Context by Language %s.', url_parts[0].part)
+        url_parts[0] = UrlPart(part=url_parts[0].part,
+                               resource=request.language)
 
     else:
         # URL is like '/{lang}/{node}/[...]/{page}[.ext]
         # Get the NodeInfo from database using path_info.
         log.debug('Get Context by NodeInfo %s.', request.path_info)
-        url_parts[-1] = (url_parts[-1][0],
-                         NodeInfo.get_by_url(request.db_session,
-                                             request.path_info))
+        url_parts[-1] = UrlPart(part=url_parts[-1].part,
+                                resource=NodeInfo.get_by_url(
+                                                    request.db_session,
+                                                    request.path_info)
+                               )
 
     # Create the resources tree.
     # The last element in resources tree is the request context.
