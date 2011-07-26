fbbb172b249ca1bcfe446171a000e399ab9c1771
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-07-26 14:54:21 +0200

    Added the Helper class and changed the BeforeRender subscriber.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110726125421-uw9ddrsu28palpw2

--- aybu/website/lib/subscriber.py
+++ aybu/website/lib/subscriber.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def add_renderer_globals(system):
-    request = system['request']    
-    return {'_': request.translate, 'localizer': request.localizer}
+from aybu.website.lib.helper import Helper
+
+def add_renderer_globals(event):
+    event['_'] = event['request'].translate
+    event['localizer'] = event['request'].localizer
+    event['h'] = Helper()
 
