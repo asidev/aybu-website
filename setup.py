e37cd8803fea142d314804ed651e7d196dfeae88
Luca Frosini <l.frosini@asidev.com>
2011-07-27 10:23:56 +0200

    Fixed dependencies
    --HG--
    extra : convert_revision : l.frosini%40asidev.com-20110727082356-m9nhsklqkhord043

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ keywords = ''
 include_package_data = True
 zip_safe = False
 requires = ['pyramid<1.2a', 'WebError', 'SQLAlchemy<0.8a', 'Pillow==1.7.2', 'Babel',
-            'recaptcha-client==1.0.6', 'aybu_themes']
-test_suite = 'aybu'
+            'recaptcha-client==1.0.6', 'aybu_themes', 'WebHelpers==1.3']
+test_suite = 'aybu-website'
 
 entry_points = """\
 [paste.app_factory]
