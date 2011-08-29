930a7071a30a6999c84675754a08056ab2ece027
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-08-29 18:22:08 +0200

    Fixed a bug related to python version in tests.py: it is backward compatible with python 2.6
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110829162208-o79xm5rovk2unz6v

--- aybu/website/tests.py
+++ aybu/website/tests.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
+try:
+    import unittest2 as unittest
+except:
+    import unittest
 
 from pyramid import testing
 from logging import getLogger
