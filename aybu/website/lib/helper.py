f388e4099684c01240897754932d55cabb894d04
Luca Frosini <l.frosini@asidev.com>
2011-08-31 11:23:46 +0200

    Moved function outside helper class
    --HG--
    extra : convert_revision : l.frosini%40asidev.com-20110831092346-puc7y0ool9vg5jl3

--- aybu/website/lib/helper.py
+++ aybu/website/lib/helper.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" ©2010-present Asidev S.r.l. """
+""" ©2010-present Asidev s.r.l. """
 
+from aybu.website.lib.common import urlify
 from aybu.website.models.language import Language
+from aybu.website.models.node import InternalLink, Node, Menu, Page, Section
 from aybu.website.models.node import ExternalLink
-from aybu.website.models.node import InternalLink
-from aybu.website.models.node import Menu
-from aybu.website.models.node import Page
-from aybu.website.models.node import Section
 from aybu.website.models.setting import Setting
 from collections import deque
 from collections import namedtuple
 from recaptcha.client.captcha import displayhtml
 from webhelpers.html.builder import literal
 import logging
-import re
 import ast
-
-__all__ = ['captcha', 'url', 'form_input', 'locale_from_language', 'urlfy',
-           'static_url']
 
 log = logging.getLogger(__name__)
 
@@ -44,13 +38,7 @@ class TemplateHelper(object):
         else:
             self._node = self._language = None
 
-
     def static_url(self, resource_url):
-        """
-        if resource_url.startswith('/uploads'):
-            return resource_url
-        """
-
         return str('/static%s' % resource_url)
 
     def url(self, url, *args, **kwargs):
@@ -111,55 +99,11 @@ class TemplateHelper(object):
         return self._translation
 
     def urlify(self, name):
-
-        xlate = {
-            0xc0: 'A', 0xc1: 'A', 0xc2: 'A', 0xc3: 'A', 0xc4: 'A', 0xc5: 'A',
-            0xc6: 'Ae', 0xc7: 'C',
-            0xc8: 'E', 0xc9: 'E', 0xca: 'E', 0xcb: 'E',
-            0xcc: 'I', 0xcd: 'I', 0xce: 'I', 0xcf: 'I',
-            0xd1: 'N',
-            0xd2: 'O', 0xd3: 'O', 0xd4: 'O', 0xd5: 'O', 0xd6: 'O',
-            0xd9: 'U', 0xda: 'U', 0xdb: 'U', 0xdc: 'U',
-            0xdd: 'Y',
-            0xe0: 'a', 0xe1: 'a', 0xe2: 'a', 0xe3: 'a', 0xe4: 'a', 0xe5: 'a',
-            0xe6: 'ae', 0xe7: 'c',
-            0xe8: 'e', 0xe9: 'e', 0xea: 'e', 0xeb: 'e',
-            0xec: 'i', 0xed: 'i', 0xee: 'i', 0xef: 'i',
-            0xf1: 'n',
-            0xf2: 'o', 0xf3: 'o', 0xf4: 'o', 0xf5: 'o', 0xf6: 'o',
-            0xf9: 'u', 0xfa: 'u', 0xfb: 'u', 0xfc: 'u',
-            0xfd: 'y', 0xff: 'y'
-        }
-
-        url = name.strip()
-
-        pattern = "\s"
-        compiled = re.compile(pattern)
-        m = compiled.search(url)
-        while m:
-            url = "%s%s%s" % (url[:m.start()], '_', url[m.end():])
-            m = compiled.search(url)
-
-        for char in url:
-            code = ord(char)
-            if code in xlate:
-                url = url.replace(char, xlate[code])
-
-        pattern = "[^a-zA-Z0-9_]"
-        compiled = re.compile(pattern)
-        m = compiled.search(url)
-        while m:
-            url = "%s%s" % (url[:m.start()], url[m.end():])
-            m = compiled.search(url)
-
-        url = url.lower()
-        url = url.strip('_')
-
-        return url
+        return urlify(name)
 
     def captcha(self, error=None):
 
-        public_key = '6LeNHcYSAAAAAKHbUX4bGAE-DE_0fR_J0nynW1OR'
+        #public_key = '6LeNHcYSAAAAAKHbUX4bGAE-DE_0fR_J0nynW1OR'
 
         html = displayhtml('6LeNHcYSAAAAAKHbUX4bGAE-DE_0fR_J0nynW1OR', error)
 
@@ -184,7 +128,6 @@ class SettingProxy(object):
             self._settings[setting.name] = setting
             self._settings[setting.name].value = value
 
-
     def __getattr__(self, attr_name):
 
         if not attr_name in self._settings:
@@ -201,10 +144,10 @@ class MenuProxy(object):
     def __init__(self, session):
         self._menus = []
         for menu in Menu.get_by_enabled(session, True):
-            self._menus.insert(menu.weight-1, NodeProxy(menu))
+            self._menus.insert(menu.weight - 1, NodeProxy(menu))
 
     def __getitem__(self, weight):
-        return self._menus[weight-1]
+        return self._menus[weight - 1]
 
 
 class NodeProxy(object):
