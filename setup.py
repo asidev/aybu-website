441e249236708b82bd487f130d6ae119f473d250
Luca Frosini <l.frosini@asidev.com>
2011-08-31 11:25:24 +0200

    Fixed dependecies and configured locatino fro extrancting message to be translated
    --HG--
    extra : convert_revision : l.frosini%40asidev.com-20110831092524-wya6p18r8lz2qyrl

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ keywords = ''
 
 include_package_data = True
 zip_safe = False
-requires = ['pyramid<1.2a', 'WebError', 'SQLAlchemy<0.8a', 'Pillow==1.7.2', 'Babel',
-            'recaptcha-client==1.0.6', 'WebHelpers==1.3']
+requires = ['pyramid<1.2a', 'WebError', 'SQLAlchemy<0.8a', 'Pillow==1.7.2',
+            'Babel', 'lingua', 'recaptcha-client==1.0.6', 'WebHelpers==1.3']
 test_suite = 'aybu.website'
 
 entry_points = """\
@@ -38,10 +38,24 @@ paster_plugins = ['pyramid']
 
 namespace_packages = ['aybu']
 
+
+message_extractors = {
+    '.': [
+        ('**.py',   'lingua_python', None ),
+    ]
+}
+
+"""
+Messages have to extracted also from
+- aybu.themes
+- instance
+"""
+
 setup(name=name, version=version, description=description,
       long_description=long_description, classifiers=classifiers,
       author=author, author_email=author_email, url=url, keywords=keywords,
       packages=find_packages(), include_package_data=include_package_data,
       zip_safe=zip_safe, install_requires=requires, tests_require=requires,
       test_suite=test_suite, entry_points=entry_points,
+      message_extractors=message_extractors,
       paster_plugins=paster_plugins, namespace_packages=namespace_packages)
