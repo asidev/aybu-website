85d17e4f6cfd29d9eb1fbeee4e1f837838ba93d8
Stefano Fontanelli <s.fontanelli@asidev.com>
2011-09-05 23:30:52 +0200

    Moved files to 'aybu.core'.
    --HG--
    extra : convert_revision : s.fontanelli%40asidev.com-20110905213052-2tmfe1em2zpba3ko

--- aybu/website/models/__init__.py
+++ aybu/website/models/__init__.py
@@ -2,25 +2,25 @@
 # -*- coding: utf-8 -*-
 
 
-from aybu.website.models.file import Banner
-from aybu.website.models.file import File
-from aybu.website.models.file import Image
-from aybu.website.models.language import Language
-from aybu.website.models.node import ExternalLink
-from aybu.website.models.node import InternalLink
-from aybu.website.models.node import Menu
-from aybu.website.models.node import Node
-from aybu.website.models.node import NodeInfo
-from aybu.website.models.node import Page
-from aybu.website.models.node import Section
-from aybu.website.models.setting import Setting
-from aybu.website.models.setting import SettingType
-from aybu.website.models.theme import Keyword
-from aybu.website.models.theme import Theme
-from aybu.website.models.user import Group
-from aybu.website.models.user import User
-from aybu.website.models.view import View
-from aybu.website.models.view import ViewDescription
+from aybu.core.models.file import Banner
+from aybu.core.models.file import File
+from aybu.core.models.file import Image
+from aybu.core.models.language import Language
+from aybu.core.models.node import ExternalLink
+from aybu.core.models.node import InternalLink
+from aybu.core.models.node import Menu
+from aybu.core.models.node import Node
+from aybu.core.models.node import NodeInfo
+from aybu.core.models.node import Page
+from aybu.core.models.node import Section
+from aybu.core.models.setting import Setting
+from aybu.core.models.setting import SettingType
+from aybu.core.models.theme import Keyword
+from aybu.core.models.theme import Theme
+from aybu.core.models.user import Group
+from aybu.core.models.user import User
+from aybu.core.models.view import View
+from aybu.core.models.view import ViewDescription
 
 
 __all__ = ['File', 'Image', 'Banner', 'Language',
