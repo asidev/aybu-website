b3341c714ec628fa3afb7c71162d16ca4666c460
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-12-22 15:52:39 +0100

    Fixed copyright and contributors

--- aybu/website/lib/__init__.py
+++ aybu/website/lib/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright 2010 Asidev s.r.l.
+Copyright 2010-2012 Asidev s.r.l.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
